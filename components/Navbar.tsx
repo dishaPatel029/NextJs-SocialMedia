a330e88cd4e2162a3414549f54941f430ff0c7d3
dishapatel <dishapatel04657@gmail.com>
2022-10-18 11:30:27 +0530

    updated: added the video functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { createOrGetUser } from "../utils";
 
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -22,6 +24,17 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
